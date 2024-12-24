0755be3ed8813834e4c7878d6b5ec866468d0fc0
futur12 <buyanafidele91@gmail.com>
2024-12-24 20:09:59 +0000

    apps

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -135,3 +135,14 @@ document.getElementById('anne').textContent = new Date().getFullYear();
 //         projectsContainer.scrollBy({ left: -320, behavior: 'smooth' });
 //     });
 // });
+
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+  }
+  
